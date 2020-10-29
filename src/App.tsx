a71d8d420968fe0372c07a5f6cc6d75ecb0c023b
Evgenia Polozova <eugpoloz@gmail.com>
2020-10-29 03:18:38 +0300

    update demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import Slider from './Slider';
 import "./App.css";
 
 function App() {
+  let [sliderValue, updateSliderValue] = React.useState(15);
+
   return (
     <div className="vk-slider-demo">
+      <h2>Controlled Slider: {sliderValue}</h2>
+
       <Slider
-        min={1}
+        min={0}
+        max={50}
+        step={1}
+        value={sliderValue}
+        onChange={updateSliderValue}
+      />
+
+      <hr />
+
+      <h2>Uncontrolled Slider</h2>
+
+      <Slider
+        min={0}
         max={100}
         step={1}
       />
